de21d6e505c0d6a0a7777dff372b675d1528489e
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-10-10 15:55:22 +0900

    Revert "[UPDATE] 잘못 푸시한 커밋 삭제"
    This reverts commit 6505e7dc6ccc02f43a220fae16ea82806b692f0b.
    
    sh: wq: command not found

--- 평균 구하기/java/hyunin/hyunin.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public static double solution(int[] arr){
-        double answer = 0;
-        for(int i = 0; i < arr.length; i++){
-            answer += arr[i];
-        }
-        return answer/arr.length;
-    }
-}
